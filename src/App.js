b3e5f4857948b8346b2c4e3191aac21cc84a86aa
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-04-25 11:35:14 +0545

    Update1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className="App">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+
+        <ul className="history">
+          
+        </ul>
+
+        <nav>
+          <p> Made by Aashish</p>
+        </nav>
+      </section>
+      
+      <section className="main"></section>
+    </div>
+  );
+}
+
+export default App;
