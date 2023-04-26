2f0b66a50c6183cd300f51af020698a6a5857e55
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-04-26 08:49:57 +0545

    Update-2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ const App = () => {
         <button>+ New Chat</button>
 
         <ul className="history">
-          
+          <li>What's up</li>
         </ul>
 
         <nav>
           <p> Made by Aashish</p>
         </nav>
       </section>
-      
-      <section className="main"></section>
+
+      <section className="main">
+        <h1>Aashish GPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            This website is created by Aashish using React just for learning
+            purposes. Any improvement feedback would be appreciated.
+          </p>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
