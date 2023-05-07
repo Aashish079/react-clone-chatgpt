f18796cd6ff4c684a83dce9ee14635efbc3354c3
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-05-08 00:37:36 +0545

    Added getMessages function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 const App = () => {
+
+  const getMessages = async () =>{
+    const options = {
+      method : "POST",
+      body : JSON.stringify({
+        message:"hello how are you?"
+      }),
+      headers: {
+        "Content-Type" : "application/json"
+      }
+
+    }
+    try{
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      console.log(data);
+    } catch(error){
+        console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <section className="side-bar">
@@ -19,7 +40,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
             <input />
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
             This website is created by Aashish using React just for learning
